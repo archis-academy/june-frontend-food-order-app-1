f99658423ff003c3fab27efd4777d45529e40a57
ygttncy <yigit03yigit@gmail.com>
2024-07-17 21:10:28 +0300

    dosya ismi düzeltme

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,7 @@ import SignupPage from "./pages/SignupPage/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import SettingsPage from "./pages/SettingsPage/SettingsPage.jsx";
-import AmodTree from "./components/AnalyticsMostOrderedDishes/Amod.jsx";
+import Dashboard from "./pages/dashboard/dashboard.jsx";
 
 const routes = [
   {
@@ -14,7 +14,7 @@ const routes = [
   {
     path: "/dashboard",
     element: (
-   <AmodTree />
+   <Dashboard />
     ),
 
   },
