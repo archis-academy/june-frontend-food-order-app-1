f9f488a3b6cb67f3be106eb41b242676d3dfdc6f
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-07-30 17:18:11 +0300

    Update component styles for ChangeButtons, CategoryTabs, SettingsHeader, ProductCard, and ProductManagment

--- src/components/ProductManagment/ProductManagment.jsx
+++ src/components/ProductManagment/ProductManagment.jsx
@@ -1,9 +1,51 @@
-import "./ProductManagment.scss";
-function ProductManagment() {
+import ProductCard from "../ProductCard/ProductCard";
+import SettingsHeader from "../SettingsHeader/SettingsHeader";
+import { foods } from "../../db/foods";
+import "../ProductManagment/ProductManagment.scss"
+import CategoryTabs from "../CategoryTabs/CategoryTabs";
+import { useState } from "react";
+import ChangeButtons from "../ChangeButtons/ChangeButtons";
+
+function ProductsManagement() {
+  const [dishes] = useState(foods);
+  const [filteredDishes, setFilteredDishes] = useState(dishes);
+  const [filterParameters, setFilterParameters] = useState({
+    orderType: "All",
+    category: "all",
+    searchQuery: "",
+  });
+
   return (
-    <div className="product-managment-container">
-      <h2>Product Managment</h2>
-    </div>
+    <>
+      <div className="management-container">
+        <div className="header-sticky">
+          <SettingsHeader />
+          <CategoryTabs
+            dishes={dishes}
+            setFilteredDishes={setFilteredDishes}
+            filterParameters={filterParameters}
+            setFilterParameters={setFilterParameters}
+          />
+        </div>
+        <div className="product-cards-container">
+          <div className="add-dish-card">
+            <span>+</span>
+            <p>Add new dish</p>
+          </div>
+          {filteredDishes.map((food) => {
+            return (
+              <ProductCard
+                key={food.id}
+                image={food.image}
+                description={food.description}
+                price={food.price}
+              />
+            );
+          })}
+        </div>
+        <ChangeButtons />
+      </div>
+    </>
   );
 }
-export default ProductManagment;
+export default ProductsManagement;
