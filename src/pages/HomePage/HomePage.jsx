c695b20f8edd5eedbcaaf05d71914f113b36ce88
ygttncy <yigit03yigit@gmail.com>
2024-08-07 22:03:24 +0300

    Dishes menu

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Header from "@/components/Header/Header";
 import "./HomePage.scss";
+import Menu from "../../components/DishesMenu/dishes";
+
+
 function HomePage() {
   return (
     <div>
       <Sidebar />
       <div className="mainRoot">
         <Header />
+       <Menu />
       </div>
-      
     </div>
   );
 }
