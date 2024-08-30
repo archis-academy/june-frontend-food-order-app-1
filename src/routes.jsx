c4faaa2ff1582536a1c4c600b557ca20ab1269f2
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-08-31 02:23:47 +0300

    Sayfa Düzenlemeleri Yapıldı

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,12 @@ import SignupPage from "./pages/SignupPage/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 import SettingsPage from "./pages/SettingsPage/SettingsPage.jsx";
 import DashBoard from "./pages/dashboard/dashboard.jsx";
+import Discount from "./pages/Discount/Discount.jsx";
+import Message from "./pages/MessagePage/Message.jsx";
+import Notification from "./pages/NotificationPage/Notification.jsx";
+
+
+
 
 
 
@@ -13,12 +19,27 @@ const routes = [
 
     element: <HomePage />,
   },
-
+  {
+    path: "/discount",
+    element: <Discount />,
+  },
+  {
+    path: "/notification",
+    element: <Notification />,
+  },
+  {
+    path: "/message",
+    element: <Message />,
+  },
   {
     path: "/dashboard",
     element: (
       <DashBoard />
     ),
+  },
+  {
+    path: "/logout",
+    element: <LoginPage />,
   },
 
   {
